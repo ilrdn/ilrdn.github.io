a3c554da60fa13dadef0f2abb4432e85821f7ad7
ilrdn <49016594+ilrdn@users.noreply.github.com>
2020-02-11 19:13:13 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    new Swiper('.swiper-container', {
+        loop: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        spaceBetween: 20,
+    });
+});
+    
