839bb782a089896a8f0339d9e673e48785f9c17e
ilrdn <49016594+ilrdn@users.noreply.github.com>
2020-02-12 16:09:49 +0400

    menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,44 @@
-$(document).ready(function() {
-    new Swiper('.swiper-container', {
-        loop: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 1,
-        spaceBetween: 20,
-    });
-});
-    
+let menu = document.getElementById("menu");
+let menuBtn = document.getElementById("menuBtn");
+let menuFlag = false;
+let search = document.getElementById("search");
+let searchBtn = document.getElementById("searchBtn");
+let searchFlag = false;
+menuBtn.onmouseover = function(event) {
+    let target = event.target;
+    target.style.filter = 'invert(100%) sepia() saturate(10000%) hue-rotate(-49deg)';
+};
+menuBtn.onmouseout = function(event) {
+    let target = event.target;
+    target.style.filter = 'invert(100%)';
+};
+searchBtn.onmouseover = function(event) {
+    let target = event.target;
+    target.style.filter = 'invert(100%) sepia() saturate(10000%) hue-rotate(-49deg)';
+};
+searchBtn.onmouseout = function(event) {
+    let target = event.target;
+    target.style.filter = 'invert(100%)';
+};
+function openMenu(){
+    if (menuFlag == false){
+        menu.style.display = "block";
+        menuFlag = true;
+        search.style.display = "none";
+        searchFlag = false;
+    } else {
+        menu.style.display = "none";
+        menuFlag = false;
+    }
+}   
+function openSearch(){
+    if (searchFlag == false){
+        search.style.display = "flex";
+        searchFlag = true;
+        menu.style.display = "none";
+        menuFlag = false;
+    } else {
+        search.style.display = "none";
+        searchFlag = false;
+    }
+}     
